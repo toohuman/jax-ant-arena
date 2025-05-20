b0f39c1a9f34e82d6f84bc9592e21df9f8d4b092
toohuman <michael.crosscombe@proton.me>
2025-05-20 11:46:54 +0900

    Moving to initial clustering analysis.

--- sync-batch.sh
+++ sync-batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Sync multirun directory from remote server
-rsync -avz michael@kodama:projects/jax-ant-arena/multirun/ multirun
+rsync -avz --include ".*" michael@kodama:projects/jax-ant-arena/multirun/ multirun
 
 echo "Sync completed!"
